909a0f7d1d7b7c300f5a435def4c5fc96d73d19a
Obi Fortune <gabrielobi.of@gmail.com>
2022-10-24 17:45:44 +0100

    Added basic info

---
+++ src/info.ts
@@ -0,0 +1,35 @@
+export const name = 'Annette Black';
+
+export const twitter = 'https://twitter.com/annetteblack';
+
+export const slackName = 'ObiFortune';
+
+export const profilePhoto = '/profile.png';
+
+export const constantLinks = [
+  {
+    id: 'btn__zuri',
+    title: 'Zuri Team',
+    link: 'https://training.zuri.team/',
+  },
+  {
+    id: 'books',
+    title: 'Zuri Books',
+    link: 'http://books.zuri.team/',
+  },
+  {
+    id: 'book__python',
+    title: 'Python Books',
+    link: `https://books.zuri.team/python-for-beginners?ref_id=${slackName}`,
+  },
+  {
+    id: 'pitch',
+    title: 'Background Check for Coders',
+    link: 'https://background.zuri.team/',
+  },
+  {
+    id: 'book__design',
+    title: 'Design Books',
+    link: 'https://books.zuri.team/design-rules',
+  },
+];
