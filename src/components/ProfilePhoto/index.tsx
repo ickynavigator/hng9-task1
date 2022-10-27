d977e2f89eab9efe876d69b6a5d31537095f3c9f
Obi Fortune <gabrielobi.of@gmail.com>
2022-10-27 19:12:47 +0100

    Added profile photo button

--- src/components/ProfilePhoto/index.tsx
+++ src/components/ProfilePhoto/index.tsx
@@ -1,9 +1,12 @@
+import { RiCameraLine } from 'react-icons/ri';
 import { profilePhoto } from '../../info';
+import styles from './index.module.scss';
 
 const ProfilePhoto: React.FC = () => {
   return (
-    <div>
-      <img id="profile__img" src={profilePhoto} alt="Profile Photo" />
+    <div className={styles.wrapper} tabIndex={1}>
+      <img id="profile__img" src={profilePhoto} className={styles.wrapper__image} alt="Profile Photo" />
+      <RiCameraLine size={25} className={styles.wrapper__camera} />
     </div>
   );
 };
