25b6e520527e4eabf3036b0cf1df6914bbd6afa5
Obi Fortune <gabrielobi.of@gmail.com>
2022-10-27 19:28:52 +0100

    Added profile and share button to the main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import LinkButton from '@Comp/LinkButton';
 import ProfilePhoto from '@Comp/ProfilePhoto';
+import ShareButton from '@Comp/ShareButton';
 import { constantLinks, name, slackName, twitter } from './info';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <ProfilePhoto />
-
-      <h2>{name}</h2>
+      <div className="profile">
+        <div className="profile__wrapper">
+          <div />
+          <ProfilePhoto />
+          <ShareButton image="share" />
+        </div>
+        <h2>{name}</h2>
+      </div>
 
       <LinkButton id={'twitter'} title={'Twitter Link'} url={twitter} />
       <LinkButton id={'slack'} title={slackName} />
