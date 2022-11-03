7c2fc83366f4dd48839e6b55d105378844a489b6
Obi Fortune <gabrielobi.of@gmail.com>
2022-11-03 13:54:14 +0100

    feat: added text inputs

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Stack, Text, Title } from '@mantine/core';
+import { Button, Checkbox, Group, Stack, Text, Textarea, TextInput, Title } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { name } from 'src/info';
 import styles from './index.module.scss';
@@ -21,6 +21,22 @@ export const Contact: React.FC = () => {
         <Text>Hi there, contact me to ask me about anything you have in mind.</Text>
 
         <Stack my={'md'}>
+          <Group grow>
+            <TextInput id="first_name" label="First Name" placeholder="Enter your first name" radius="md" />
+            <TextInput id="last_name" label="Last Name" placeholder="Enter your last name" radius="md" />
+          </Group>
+
+          <TextInput id="email" label="Email" placeholder="yourname@email.com" radius="md" type="email" />
+
+          <Textarea
+            id="message"
+            label="Message"
+            placeholder="Send me a message and I'll reply you as soon as possible..."
+            minRows={4}
+            maxRows={4}
+            radius="md"
+          />
+
           <Checkbox label={`You agree to providing your data to ${name} who may contact you.`} />
         </Stack>
 
