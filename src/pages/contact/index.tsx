64e864ea3f7fdba559de580735d4725525c87e1e
Obi Fortune <gabrielobi.of@gmail.com>
2022-11-03 13:41:07 +0100

    feat: created form in the contact page

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,5 +1,35 @@
+import { Button, Checkbox, Stack, Text, Title } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { name } from 'src/info';
+import styles from './index.module.scss';
+
 export const Contact: React.FC = () => {
-  return <div>contact</div>;
+  const form = useForm({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      message: '',
+      TOS: false,
+    },
+  });
+
+  return (
+    <div className={styles.contact}>
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <Title order={1}>Contact Me</Title>
+        <Text>Hi there, contact me to ask me about anything you have in mind.</Text>
+
+        <Stack my={'md'}>
+          <Checkbox label={`You agree to providing your data to ${name} who may contact you.`} />
+        </Stack>
+
+        <Button fullWidth type="submit" radius={'md'} id="btn_submit">
+          Send Message
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
