e35c1685d23294a34bcd598cae5060c84f9d26d2
Obi Fortune <gabrielobi.of@gmail.com>
2022-11-03 14:00:43 +0100

    feat: footer in contact

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,3 +1,4 @@
+import Footer from '@Comp/Footer';
 import { Button, Checkbox, Group, Stack, Text, Textarea, TextInput, Title } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { name } from 'src/info';
@@ -15,36 +16,35 @@ export const Contact: React.FC = () => {
   });
 
   return (
-    <div className={styles.contact}>
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
-        <Title order={1}>Contact Me</Title>
-        <Text>Hi there, contact me to ask me about anything you have in mind.</Text>
+    <>
+      <div className={styles.contact}>
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <Title order={1}>Contact Me</Title>
+          <Text>Hi there, contact me to ask me about anything you have in mind.</Text>
+          <Stack my={'md'}>
+            <Group grow>
+              <TextInput id="first_name" label="First Name" placeholder="Enter your first name" radius="md" />
+              <TextInput id="last_name" label="Last Name" placeholder="Enter your last name" radius="md" />
+            </Group>
+            <TextInput id="email" label="Email" placeholder="yourname@email.com" radius="md" type="email" />
+            <Textarea
+              id="message"
+              label="Message"
+              placeholder="Send me a message and I'll reply you as soon as possible..."
+              minRows={4}
+              maxRows={4}
+              radius="md"
+            />
+            <Checkbox label={`You agree to providing your data to ${name} who may contact you.`} />
+          </Stack>
+          <Button fullWidth type="submit" radius={'md'} id="btn_submit">
+            Send Message
+          </Button>
+        </form>
+      </div>
 
-        <Stack my={'md'}>
-          <Group grow>
-            <TextInput id="first_name" label="First Name" placeholder="Enter your first name" radius="md" />
-            <TextInput id="last_name" label="Last Name" placeholder="Enter your last name" radius="md" />
-          </Group>
-
-          <TextInput id="email" label="Email" placeholder="yourname@email.com" radius="md" type="email" />
-
-          <Textarea
-            id="message"
-            label="Message"
-            placeholder="Send me a message and I'll reply you as soon as possible..."
-            minRows={4}
-            maxRows={4}
-            radius="md"
-          />
-
-          <Checkbox label={`You agree to providing your data to ${name} who may contact you.`} />
-        </Stack>
-
-        <Button fullWidth type="submit" radius={'md'} id="btn_submit">
-          Send Message
-        </Button>
-      </form>
-    </div>
+      <Footer />
+    </>
   );
 };
 
